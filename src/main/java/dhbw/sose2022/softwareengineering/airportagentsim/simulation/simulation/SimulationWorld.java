00d96f644245c39add1d1327a94f2c788bf69a73
lelek_develop <jonathanlelek@aol.de>
2022-05-17 13:00:07 +0200

    JL_2022-05-17: simulation world

--- src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/simulation/SimulationWorld.java
+++ src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/simulation/SimulationWorld.java
@@ -6,41 +6,43 @@ import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulati
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity.Entity;
 
 public final class SimulationWorld implements World {
+	public int width;
+	public int height;
+	public Collection<Entity> entities;
 	
+	public SimulationWorld(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
 	@Override
 	public int getWidth() {
-		// TODO implement
-		throw new UnsupportedOperationException();
+		return this.width;
 	}
 	
 	@Override
 	public int getHeight() {
-		// TODO implement
-		throw new UnsupportedOperationException();
+		return this.height;
 	}
 	
 	@Override
 	public Collection<Entity> getEntities() {
-		// TODO implement
-		throw new UnsupportedOperationException();
+		return this.entities;
 	}
 	
 	@Override
 	public void add(Entity e) {
-		// TODO implement
-		throw new UnsupportedOperationException();
+		this.entities.add(e);
 	}
 	
 	@Override
 	public void addAll(Collection<? extends Entity> c) {
-		// TODO implement
-		throw new UnsupportedOperationException();
+		this.entities = c;
 	}
 	
 	@Override
 	public void remove(Entity e) {
-		// TODO implement
-		throw new UnsupportedOperationException();
+		this.entities.remove(e);
 	}
 	
 }
