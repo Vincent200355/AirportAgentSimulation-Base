8c8eeda3336ad0aa51de1fcafa14a4fe3f58adaa
DavidFambach <david.fambach@gmx.de>
2022-05-18 08:21:16 +0200

    Fixed file error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,28 +1,4 @@
 module dhbw.sose2022.softwareengineering.AASBase {
-<<<<<<<HEAD
-	
-	requires java.base;
-	requires java.logging;
-	requires java.desktop;
-	
-	requires transitive org.apache.logging.log4j;
-	
-	requires org.apache.commons.lang3;
-	requires org.apache.logging.log4j.core;
-	
-	requires simple.xml;
-	requires json.simple;
-
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api;
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.config;
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.geometry;
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.logging;
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.plugin;
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation;
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity;
-    exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.message;
-	
-=======
 
     requires java.base;
     requires java.logging;
@@ -30,12 +6,10 @@ module dhbw.sose2022.softwareengineering.AASBase {
 
     requires transitive org.apache.logging.log4j;
 
+    requires gson;
+
     requires org.apache.commons.lang3;
     requires org.apache.logging.log4j.core;
-
-    requires simple.xml;
-    requires json.simple;
-    requires gson;
 
     exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api;
     exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.geometry;
@@ -44,6 +18,4 @@ module dhbw.sose2022.softwareengineering.AASBase {
     exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation;
     exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity;
     exports dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.message;
-
->>>>>>>ddad726(.gitignore)
 }
