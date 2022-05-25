2c9f8f9630416083d657ddb466dc3917786f0366
Vincent Hundeloh <s200355@student.dhbw-mannheim.de>
2022-05-25 14:21:30 +0200

    Updated entity spawning
    + Added error handling for invalid configured world size
    + Added documentation for API World
    + Added documentation for API Entity
    + Added API Entity.onBirth() and Entity.onDeath()
    + Implemented Entity.kill()
    ~ Deprecated add(Entity), addAll(Collection) and remove(Entity) in API world
    ~ Fixed a bug that entities are not spawned in the world after loading them from the configuration
    ~ Protected SimulationWorld.entities against external modification

--- src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/api/simulation/World.java
+++ src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/api/simulation/World.java
@@ -7,16 +7,60 @@ import dhbw.sose2022.softwareengineering.airportagentsim.simulation.simulation.S
 
 public sealed interface World permits SimulationWorld {
 	
+	/**
+	 * The width of the world, which is always positive.<br><br>
+	 * 
+	 * @return the width of the world
+	 */
 	public int getWidth();
 	
+	/**
+	 * The height of the world, which is always positive.<br><br>
+	 * 
+	 * @return the height of the world
+	 */
 	public int getHeight();
 	
+	/**
+	 * Returns a newly allocated list of all entities of this world.<br><br>
+	 * 
+	 * @return a list of all entities
+	 */
 	public Collection<Entity> getEntities();
 	
+	/**
+	 * Adds an entity to this world. The entity will be spawned at x=0, y=0 and
+	 * will have a width and height of 1.
+	 * <b>{@link Entity#spawn(World, int, int, int, int)} should be used
+	 * instead.</b><br><br>
+	 * 
+	 * @param e the entity
+	 */
+	@Deprecated
 	public void add(Entity e);
 	
+	/**
+	 * Adds the entities to this world. The entities are assigned a number i
+	 * according to their position in the iteration order of the given
+	 * {@link Collection}. The i-th entity will be spawned at x=i%width,
+	 * y=i/width, where width is the width of this world. All entities will have
+	 * a width and height of 1. If this method fails, it may do so after adding
+	 * some, but not all, of the entities.
+	 * <b>{@link Entity#spawn(World, int, int, int, int)} should be used
+	 * instead.</b><br><br>
+	 * 
+	 * @param c a collection of entities
+	 */
+	@Deprecated
 	public void addAll(Collection<? extends Entity> c);
 	
+	/**
+	 * Removes an entity from this world. <b>{@link Entity#kill()} should be
+	 * used instead.</b><br><br>
+	 * 
+	 * @param e the entity
+	 */
+	@Deprecated
 	public void remove(Entity e);
 	
 }
--- src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/simulation/SimulationWorld.java
+++ src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/simulation/SimulationWorld.java
@@ -2,7 +2,10 @@ package dhbw.sose2022.softwareengineering.airportagentsim.simulation.simulation;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 
+import org.apache.commons.lang3.Validate;
 import org.apache.logging.log4j.Logger;
 
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.AirportAgentSim;
@@ -20,6 +23,15 @@ public final class SimulationWorld implements World {
 	private final int height;
 	private final ArrayList<Entity> entities = new ArrayList<Entity>();
 	
+	/**
+	 * Creates a new simulation world. Both the width and the height must be
+	 * positive.<br><br>
+	 * 
+	 * @param aas the simulation
+	 * @param logger the logger to use
+	 * @param width the width of the world
+	 * @param height the height of the world
+	 */
 	public SimulationWorld(AirportAgentSim aas, Logger logger, int width, int height) {
 		this.aas = aas;
 		this.logger = logger;
@@ -39,21 +51,47 @@ public final class SimulationWorld implements World {
 	
 	@Override
 	public Collection<Entity> getEntities() {
-		return this.entities;
+		ArrayList<Entity> list = new ArrayList<Entity>();
+		list.addAll(this.entities);
+		return list;
 	}
 	
 	@Override
+	@Deprecated
 	public void add(Entity e) {
+		Validate.notNull(e);
+		e.spawn(this, 0, 0, 1, 1);
+	}
+	
+	@Override
+	@Deprecated
+	public void addAll(Collection<? extends Entity> c) {
+		Validate.notNull(c);
+		Validate.isTrue(c.size() <= this.width * this.height, "Cannot fit entities in world");
+		Iterator<? extends Entity> iterator = c.iterator();
+		for(int y = 0; y < this.height; y++) {
+			for(int x = 0; x < this.width; x++) {
+				if(!iterator.hasNext())
+					return;
+				Entity e = iterator.next();
+				Validate.notNull(e);
+				e.spawn(this, x, y, 1, 1);
+			}
+		}
+	}
+	
+	@Override
+	@Deprecated
+	public void remove(Entity e) {
+		Validate.notNull(e);
+		e.kill();
+	}
+	
+	public void addEntity(Entity e) {
 		this.entities.add(e);
 	}
 	
-	@Override
-	public void addAll(Collection<? extends Entity> c) {
-		this.entities.addAll(c);
-	}
-	
-	@Override
-	public void remove(Entity e) {
+	public void removeEntity(Entity e) {
 		this.entities.remove(e);
 	}
 	
