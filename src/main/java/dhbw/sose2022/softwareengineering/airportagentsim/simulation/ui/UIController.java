36d00c93725655c20b0b8560ddd747204201bf9b
DavidFambach <david.fambach@gmx.de>
2022-06-08 18:34:01 +0200

    + Entities show up before simulation starts

--- src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/ui/UIController.java
+++ src/main/java/dhbw/sose2022/softwareengineering/airportagentsim/simulation/ui/UIController.java
@@ -86,6 +86,7 @@ public class UIController {
         initializeLibrary();
         initializeSimulationObjects();
         initializeView();
+        updateUI();
     }
 
     private void initializeSpeedSlider() {
